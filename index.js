d0c709d0d8a32b278ffc0ba44e0bc43c48185ce7
Yasen30 <mdyasenrafe@gmail.com>
2021-10-27 22:38:04 +0600

    first commit-

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("<h1>This is Volunteer network server</h1>");
+});
+
+app.listen(port, () => {
+  console.log("sucessfully by run", port);
+});
